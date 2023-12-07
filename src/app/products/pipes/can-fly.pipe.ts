845a75868e7c9b58a0a8433f400c93cb8aead097
gaburra-m <gaburra.m@gmail.com>
2023-12-06 18:37:11 -0600

    Proyecto terminado con los pipes presonlizados

---
+++ src/app/products/pipes/can-fly.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'canFly',
+})
+export class CanFlyPipe implements PipeTransform {
+  transform(value: boolean): 'vuela' | 'no vuela' {
+    return value === true ? 'vuela' : 'no vuela';
+  }
+}
